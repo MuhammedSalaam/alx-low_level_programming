174d858845953ffb52ebdbda47909687ee550589
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-09-09 20:36:47 +0100

    task done

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,51 +1,17 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  *  main - prints the alphabet in lowercase, then in uppercase.
- *
- *   *
- *
- *    *   Return: 0 on success
- *
- *     */
-
-
-
+ * main - This program prints a-z to std output
+ * Return: Exit status is zero if successful
+ */
 int main(void)
-
 {
+	int i;
 
-		char c = 'a';
-
-			char d = 'A';
-
-
-
-				while (c <= 'z')
-
-						{
-
-									putchar(c);
-
-											c++;
-
-												}
-
-					while (d <= 'Z')
-
-							{
-
-										putchar(d);
-
-												d++;
-
-													}
-
-						putchar('\n');
-
-							return (0);
-
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
