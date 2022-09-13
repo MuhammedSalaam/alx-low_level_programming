bbaeabae79e48af30b6e00a2f8c5f098313921d3
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-09-13 22:23:51 +0100

    task done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,41 +1,32 @@
- #include "main.h"
-
-
+#include "main.h"
 
 /**
+ * jack_bauer - prints 00:00 to 23:59
  *
- *  * main - Entry point
- *
- *   * Return:always 0 (success)
- *
- *    */
-
-int main(void)
-
+ * Return: Just prints
+ */
+void jack_bauer(void)
 {
+	int i, j, k, l;
 
-		_putchar('_');
-
-			_putchar('p');
-
-				_putchar('u');
-
-					_putchar('t');
-
-						_putchar('c');
-
-							_putchar('h');
-
-								_putchar('a');
-
-									_putchar('r');
-
-										_putchar('\n');
-
-
-
-											return (0);
-
+	for (i = 48; i < 51; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k < 54; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+			if (i == 50 && j > 50)
+				break;
+		}
+	}
 }
-
-
