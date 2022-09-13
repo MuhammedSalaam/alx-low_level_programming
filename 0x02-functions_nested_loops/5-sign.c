5430a81e75a88efb4e23f7f86154f82d9b8ced88
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-09-13 23:05:52 +0100

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- * print_sign - Check for -ve, +ve or 0 integers
- * @n: This is a real number
- *
- * Return: 0 if zero, 1 if > 0 and -1 if < 0
+ * print_sign - function that prints the sign of a number.
+ * @n: the number to be checked.
+ * Return: 1 if positive, 0 if zero, -1 if negative.
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -13,14 +12,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	else
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
 	}
 }
