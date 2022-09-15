4bcfc85e9e7945e318effb089a41efdc027ccd05
muhammedsalaam <salaam.muhammed@gmail.com>
2022-09-15 13:10:42 -0700

    task done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+#include <ctype.h>
+
+
+
+/**
+ *
+ *  * _isupper - checks for uppercase letters
+ *
+ *   *
+ *
+ *    * @c: parametr to be printed
+ *
+ *     * Return: 0 or 1 depending on the condition
+ *
+ *      */
+
+
+
+int _isupper(int c)
+
+{
+
+		if (isupper(c))
+
+				{
+
+							return (1);
+
+								}
+
+			else
+
+					{
+
+								return (0);
+
+									}
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
