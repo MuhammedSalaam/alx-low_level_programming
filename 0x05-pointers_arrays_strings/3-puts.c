31d921880b753ff9e7db32311df44dde3d3e7318
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-09-19 14:55:52 +0100

    task done

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,33 +1,19 @@
-#ifndef MAIN_H
+#include "main.h"
 
-#define MAIN_H
+/**
+ * _puts - prints a string to stdout
+ * @str: this is a string
+ *
+ * Return: Null void
+ */
+void _puts(char *str)
+{
+	int track;
 
+	for (track = 0; *(str + track) != '\0'; track++)
+	{
+		_putchar(*(str + track));
+	}
 
-
-int _putchar(char c);
-
-void reset_to_98(int *n);
-
-void swap_int(int *a, int *b);
-
-int _strlen(char *s);
-
-void _puts(char *str);
-
-void print_rev(char *s);
-
-void rev_string(char *s);
-
-void puts2(char *str);
-
-void puts_half(char *str);
-
-void print_array(int *a, int n);
-
-char *_strcpy(char *dest, char *src);
-
-int _atoi(char *s);
-
-
-
-#endif
+	_putchar('\n');
+}
