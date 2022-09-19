d9797970467df3c6df7b06112d09b14cd2ba7919
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-09-19 14:48:58 +0100

    task done

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,23 +1,12 @@
 #include "main.h"
 
-
-
 /**
+ * reset_to_98 - Resets the value of the memory location it accepts to 98
+ * @n: This is a pointer
  *
- *  * reset_to_98 - Resets the value of the memory location it accepts to 98
- *
- *   * @n: This is a pointer
- *
- *    *
- *
- *     * Return: Null Void
- *
- *      */
-
+ * Return: Null Void
+ */
 void reset_to_98(int *n)
-
 {
-
-		*n = 98;
-
+	*n = 98;
 }
