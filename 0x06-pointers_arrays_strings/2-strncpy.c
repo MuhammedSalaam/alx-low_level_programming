2dc997861f6b644ac2f5cedbd884e05b0ecfa410
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-09-21 15:01:56 +0100

    task done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,59 +1,26 @@
 #include "main.h"
 
-
-
 /**
+ * _strncpy - Concatenates one string to another
+ * @dest: This is the string to be copied to
+ * @src: This is the string to be copied
+ * @n: number of bytes to be copied
  *
- *  * _strncat - Concatenates one string to another
- *
- *   * @dest: This is the string to be concatenated to
- *
- *    * @src: This is the string to be concatenated
- *
- *     * @n: number of bytes to be copied
- *
- *      *
- *
- *       * Return: The destination string
- *
- *        */
-
-char *_strncat(char *dest, char *src, int n)
-
+ * Return: The destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
-		int i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
 
-			int destLength = 0;
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 
-				int counter = 0;
-
-
-
-					while (dest[counter] != '\0')
-
-							{
-
-										destLength++;
-
-												counter++;
-
-													}
-
-
-
-						for (i = 0; i < n && src[i] != '\0'; i++)
-
-								{
-
-											dest[destLength + i] = src[i];
-
-												}
-
-							dest[destLength + i] = '\0';
-
-
-
-								return (dest);
-
+	return (dest);
 }
