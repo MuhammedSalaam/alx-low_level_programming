c9291a9f0908d2918df9c96f778477fa058cd97f
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-09-21 15:15:04 +0100

    task done

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,59 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-
-
-/**
- *
- *  * _strncat - Concatenates one string to another
- *
- *   * @dest: This is the string to be concatenated to
- *
- *    * @src: This is the string to be concatenated
- *
- *     * @n: number of bytes to be copied
- *
- *      *
- *
- *       * Return: The destination string
- *
- *        */
-
-char *_strncat(char *dest, char *src, int n)
-
+int main(void)
 {
+  int n;
+  int a[5];
+  int *p;
 
-		int i;
-
-			int destLength = 0;
-
-				int counter = 0;
-
-
-
-					while (dest[counter] != '\0')
-
-							{
-
-										destLength++;
-
-												counter++;
-
-													}
-
-
-
-						for (i = 0; i < n && src[i] != '\0'; i++)
-
-								{
-
-											dest[destLength + i] = src[i];
-
-												}
-
-							dest[destLength + i] = '\0';
-
-
-
-								return (dest);
-
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  p[5] = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
