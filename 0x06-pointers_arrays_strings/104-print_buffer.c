c8a643e74c98d39f86a1d380d53e47ed13729b8a
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-09-21 15:17:09 +0100

    task done

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,59 +1,43 @@
 #include "main.h"
-
-
+#include <stdio.h>
 
 /**
- *
- *  * _strncat - Concatenates one string to another
- *
- *   * @dest: This is the string to be concatenated to
- *
- *    * @src: This is the string to be concatenated
- *
- *     * @n: number of bytes to be copied
- *
- *      *
- *
- *       * Return: The destination string
- *
- *        */
-
-char *_strncat(char *dest, char *src, int n)
-
+ * print_buffer - prints a buffer
+ * @b: buffer
+ * @size: size of the buffer
+ * Return: Null void
+ */
+void print_buffer(char *b, int size)
 {
+	int j, k, l;
 
-		int i;
-
-			int destLength = 0;
-
-				int counter = 0;
-
-
-
-					while (dest[counter] != '\0')
-
-							{
-
-										destLength++;
-
-												counter++;
-
-													}
-
-
-
-						for (i = 0; i < n && src[i] != '\0'; i++)
-
-								{
-
-											dest[destLength + i] = src[i];
-
-												}
-
-							dest[destLength + i] = '\0';
-
-
-
-								return (dest);
-
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
+	}
 }
