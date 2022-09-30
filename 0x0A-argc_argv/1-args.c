93bfc0780d08c4965c236679ea138031f82a8d7b
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-09-30 08:31:35 +0100

    task done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,16 @@
-wwl,ml,mlom,[lo,mlo
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
