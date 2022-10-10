08ab27736475502cb4d09c35f880add10056852d
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-10-10 15:06:54 +0100

    task done

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,12 @@
-ms ms 2m s m
+#include <stdio.h>
+
+/**
+ * main - Prints the name of whatever file runs this code
+ *
+ * Return: Null Void
+ */
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
