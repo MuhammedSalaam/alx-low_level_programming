4d65a1e2d6544879421b127e19760db47fd9f75d
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-10-25 16:15:43 +0100

    TASK DONE

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,20 @@
-idji3j3ik
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h: pointer to the head of a list
+ *
+ * Return: numbers of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		nnodes++;
+		h = h->next;
+	}
+	return (nnodes);
+}
