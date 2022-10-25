fc4e741f9a6fa2d779dc0d3c27663c6f167830e2
Engr Muhammed Salaam <109607147+MuhammedSalaam@users.noreply.github.com>
2022-10-25 16:19:14 +0100

    TASK DONE

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,18 @@
-gftfgtfgtyfgvtf
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to the head of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
